fb9866dad7a9e153e065da088a4db10ee8f14b63
= <=>
2026-06-25 13:27:21 -0300

    test: espelha estrutura modular e corrige paths para os testes passarem

--- lib/features/vaccines/services/vaccine_service.dart
+++ lib/features/vaccines/services/vaccine_service.dart
@@ -4,7 +4,7 @@ import 'package:flutter/services.dart';
 import 'package:cinco_tigres_felizes/features/vaccines/models/vaccine_model.dart';
 
 class VacinasService {
-  static const String _vacinasAssetPath = 'lib/features/vaccines/assets/vacinas.json';
+  static const String _vacinasAssetPath = 'lib/features/vaccines/data/vacinas.json';
 
   Future<VacinasCalendarioModel> carregarVacinas() async {
     final jsonString = await rootBundle.loadString(_vacinasAssetPath);
