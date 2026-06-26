624987a2c0ac5a357addd91c1ddf266bc5fa8584
= <=>
2026-06-25 23:19:45 -0300

    refactor(vaccines): atualiza registro de providers na main para novo contrato

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:cinco_tigres_felizes/features/access/presentation/pages/home_page.dart';
 import 'package:provider/provider.dart';
 import 'features/habits/services/hydration_service.dart';
+import 'package:cinco_tigres_felizes/features/vaccines/data/repositories/vaccines_repository.dart';
+import 'package:cinco_tigres_felizes/features/vaccines/domain/repositories/i_vaccines_repository.dart';
 import 'package:cinco_tigres_felizes/features/vaccines/services/vaccine_service.dart';
 
 void main() {
@@ -9,7 +11,15 @@ void main() {
     MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (context) => HidratacaoService()),
-        ChangeNotifierProvider(create: (context) => VacinasService()),
+
+        // Substituir o ChangeNotifierProvider antigo por estes dois:
+        Provider<IVacinasRepository>(
+          create: (_) => VacinasRepository(),
+        ),
+        ChangeNotifierProxyProvider<IVacinasRepository, VacinasService>(
+          create: (ctx) => VacinasService(ctx.read<IVacinasRepository>()),
+          update: (_, repo, previous) => previous ?? VacinasService(repo),
+        ),
       ],
       child: const MyApp(),
     ),
